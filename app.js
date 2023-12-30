85328f0ce342c8fa15f501034fbede8565732911
eldad-assaf <eldadux@gmail.com>
2023-12-30 14:55:13 +0200

    morgan

--- app.js
+++ app.js
@@ -1,35 +1,63 @@
-require('dotenv').config()
-require('express-async-errors')
-const mongoose = require('mongoose'); 
+// Load environment variables from the .env file
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
+// Handle asynchronous errors in Express
+require('express-async-errors');
+
+// Import required modules
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+// Initialize Express app
+const app = express();
+
+// Middleware for logging requests
+app.use(morgan('tiny'));
+
+// Middleware to parse JSON in requests
+app.use(express.json());
+
 // Set Mongoose option to suppress deprecation warning
 mongoose.set('strictQuery', false);
-console.log(process.env)
 
-//Database
-const connectDB = require('./db/connect.js')
-//middleware
 
-const notFoundMiddleware = require('./middleware/not-found.js')
-const errorHandlerMiddleware = require('./middleware/error-handler.js')
 
-app.use(express.json())
+// Connect to the MongoDB database
+const connectDB = require('./db/connect.js');
+
+// Import middleware
+const notFoundMiddleware = require('./middleware/not-found.js');
+const errorHandlerMiddleware = require('./middleware/error-handler.js');
+
+// Define a route for the home page
 app.get('/', (req, res) => {
-  res.send('e-commerce api!')
-})
+  res.send('e-commerce API!');
+});
 
+// Use custom middleware for handling 404 errors
 app.use(notFoundMiddleware);
+
+// Use custom middleware for handling errors
 app.use(errorHandlerMiddleware);
-const port = process.env.PORT || 3005
-const start = async ()=> {
+
+// Define the port for the server to listen on
+const port = process.env.PORT || 3005;
+
+// Define an asynchronous function to start the server
+const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL)
-    app.listen(port, ()=> {
-      console.log(`Server running on port ${port}`)
-    })
+    // Connect to the MongoDB database
+    await connectDB(process.env.MONGO_URL);
+
+    // Start the Express server
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
   } catch (error) {
-console.log(error)  }
-}
-start()
+    console.error(error);
+  }
+};
+
+// Call the start function to initiate the server
+start();
