e03c25ce4967f8c429d658e55ce535d6ad5dc0fc
eldad-assaf <eldadux@gmail.com>
2023-12-30 14:43:17 +0200

    save

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('express-async-errors')
 const mongoose = require('mongoose'); 
 
 const express = require('express')
@@ -9,7 +10,18 @@ console.log(process.env)
 
 //Database
 const connectDB = require('./db/connect.js')
+//middleware
 
+const notFoundMiddleware = require('./middleware/not-found.js')
+const errorHandlerMiddleware = require('./middleware/error-handler.js')
+
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('e-commerce api!')
+})
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3005
 const start = async ()=> {
   try {
